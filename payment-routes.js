8566cffbc515c8eaab35a9c64a4429d0644bbdd0
Augustine <austinedoho@gmail.com>
2026-08-24 13:25:24 +0000

    Fix UUID import - use native crypto instead

--- payment-routes.js
+++ payment-routes.js
@@ -4,8 +4,16 @@
  */
 
 const express = require('express');
-const { v4: uuidv4 } = require('uuid');
-const paystack = require('./paystack-integration');
+const crypto = require('crypto');
+
+// Function to generate UUID
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0,
+      v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}const paystack = require('./paystack-integration');
 
 const router = express.Router();
 
